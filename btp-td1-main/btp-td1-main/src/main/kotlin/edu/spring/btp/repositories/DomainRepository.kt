3403ec6f71ee87f00481509d93adfc1c1cea8426
Xozez <120087607+Xozez@users.noreply.github.com>
2023-03-21 08:51:53 +0100

    Creation des requetes pour les repositories

--- btp-td1-main/btp-td1-main/src/main/kotlin/edu/spring/btp/repositories/DomainRepository.kt
+++ btp-td1-main/btp-td1-main/src/main/kotlin/edu/spring/btp/repositories/DomainRepository.kt
@@ -11,6 +11,19 @@ interface DomainRepository:JpaRepository<Domain, Int> {
     @Query(nativeQuery = true,value="SELECT * FROM \"domain\" INNER JOIN \"domain_providers\" ON \"providers_id\"=:providerId ORDER BY rand() LIMIT 1")
     fun getRandomDomainFromProvider(providerId:Int): Domain
 
+
+    @Query(nativeQuery = true,value="SELECT * FROM \"domain\" WHERE \"name\"=:name")
+    fun findByName(name:String):Domain
+
+    @Query(nativeQuery = true,value="SELECT * FROM \"domain\" WHERE \"parent_id\"=(SELECT \"id\" FROM \"domain\" WHERE \"name\"=:name)")
     fun findByParentName(name:String):List<Domain>
 
+    @Query(nativeQuery = true,value="SELECT * FROM \"domain\" WHERE \"parent_id\" IS NULL")
+    fun finByParentIsNull():List<Domain>
+
+    @Query(nativeQuery = true,value="SELECT * FROM \"domain\" WHERE \"parent_id\"=:parentId")
+    fun findByParentId(parentId:Int):List<Domain>
+
+    @Query(nativeQuery = true,value="SELECT * FROM \"domain\" WHERE \"parent_id\"=:parentId ORDER BY rand() LIMIT 1")
+    fun findRandomByParentId(parentId:Int):Domain?
 }
--- btp-td1-main/btp-td1-main/src/main/kotlin/edu/spring/btp/repositories/UserRepository.kt
+++ btp-td1-main/btp-td1-main/src/main/kotlin/edu/spring/btp/repositories/UserRepository.kt
@@ -7,4 +7,10 @@ import org.springframework.data.jpa.repository.Query
 interface UserRepository: JpaRepository<User, Int> {
     @Query(nativeQuery = true,value="SELECT * FROM \"user\" ORDER BY rand() LIMIT 1")
     fun getRandomUser(): User
+
+    @Query(nativeQuery = true,value="SELECT * FROM \"user\" WHERE \"email\"=:email")
+    fun findByEmail(email:String):User
+
+    @Query(nativeQuery = true,value="SELECT * FROM \"user\" WHERE \"username\"=:username")
+    fun findByUsername(username:String):User
 }
