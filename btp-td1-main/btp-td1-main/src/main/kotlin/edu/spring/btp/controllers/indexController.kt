19e14db2c5509d3f9c96e74f77d4eefe0bd8f772
Xozez <120087607+Xozez@users.noreply.github.com>
2023-03-21 11:01:04 +0100

    Ajustement des pages crées + debut securité

--- btp-td1-main/btp-td1-main/src/main/kotlin/edu/spring/btp/controllers/indexController.kt
+++ btp-td1-main/btp-td1-main/src/main/kotlin/edu/spring/btp/controllers/indexController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @RequestMapping("/")
 @Controller
-class indexController {
+class IndexController {
 
     @Autowired
     lateinit var userRepository: UserRepository
